960a212e0a249177a6ac66528f1d4b2147ab75cf
Boranbayev Danial <1.dani.qwerty@gmail.com>
2025-05-20 23:15:05 +0500

    add main method to demonstrate BFS and Dijkstra's algorithm on a weighted graph

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        WeightedGraph<String> graph = new WeightedGraph<>();
+        Vertex<String> v0 = new Vertex<>("A");
+        Vertex<String> v1 = new Vertex<>("B");
+        Vertex<String> v2 = new Vertex<>("C");
+        Vertex<String> v3 = new Vertex<>("D");
+
+        graph.addEdge(v0, v1, 1.0);
+        graph.addEdge(v0, v2, 2.0);
+        graph.addEdge(v1, v2, 1.0);
+        graph.addEdge(v2, v3, 3.0);
+        graph.addEdge(v1, v3, 4.0);
+
+        Search<String> bfs = new BreadthFirstSearch<>();
+        bfs.search(graph, v0);
+        System.out.println("BFS Path from A to D: " + bfs.pathTo(v3));
+
+        Search<String> dijkstra = new DijkstraSearch<>();
+        dijkstra.search(graph, v0);
+        System.out.println("Dijkstra Path from A to D: " + dijkstra.pathTo(v3));
     }
-} 
+}
